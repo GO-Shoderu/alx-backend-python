50064c3860d37331e02afcfea11fca0c6719be23
GO-Shoderu <go.shoderu@gmail.com>
2025-11-09 00:52:15 +0200

    Context Manager Task 2 - fix

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -4,40 +4,35 @@ import aiosqlite
 
 DB_PATH = "example.db"
 
-
-async def async_fetch_users(db_path: str = DB_PATH):
+async def async_fetch_users():
     
-    async with aiosqlite.connect(db_path) as db:
+    async with aiosqlite.connect(DB_PATH) as db:
         db.row_factory = sqlite3.Row
         async with db.execute("SELECT * FROM users") as cur:
             rows = await cur.fetchall()
             return rows
 
-
-async def async_fetch_older_users(db_path: str = DB_PATH, min_age: int = 40):
-    async with aiosqlite.connect(db_path) as db:
+async def async_fetch_older_users():
+    
+    async with aiosqlite.connect(DB_PATH) as db:
         db.row_factory = sqlite3.Row
-        async with db.execute("SELECT * FROM users WHERE age > ?", (min_age,)) as cur:
+        async with db.execute("SELECT * FROM users WHERE age > ?", (40,)) as cur:
             rows = await cur.fetchall()
             return rows
 
-
-async def fetch_concurrently(db_path: str = DB_PATH):
-    
-    all_users_task = async_fetch_users(db_path)
-    older_users_task = async_fetch_older_users(db_path, 40)
-
-    all_users, older_users = await asyncio.gather(all_users_task, older_users_task)
+async def fetch_concurrently():
+    all_users, older_users = await asyncio.gather(
+        async_fetch_users(),
+        async_fetch_older_users()
+    )
 
     print("All users:")
     for r in all_users:
-        # Works because of row_factory = sqlite3.Row
-        print(f"- id={r['id']}, name={r['name']}, age={r.get('age')}, email={r['email']}")
+        print(f"- id={r['id']}, name={r['name']}, age={r['age']}, email={r['email']}")
 
     print("\nUsers older than 40:")
     for r in older_users:
         print(f"- id={r['id']}, name={r['name']}, age={r['age']}, email={r['email']}")
-
 
 if __name__ == "__main__":
     asyncio.run(fetch_concurrently())
