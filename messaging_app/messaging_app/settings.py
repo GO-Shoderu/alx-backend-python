35776f9f5e15da124939ff9f40543567a2eafeb9
GO-Shoderu <go.shoderu@gmail.com>
2025-11-15 22:23:18 +0200

    Adjusting the settngs file

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-party apps
+    'rest_framework',
+
+    # Local apps
+    'chats',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +126,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Django REST Framework configuration
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+}
